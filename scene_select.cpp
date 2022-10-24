558c2e5f1458ef65dc44648a57ddc4011600977d
mame0622 <mame.like.ww@gmail.com>
2022-10-24 16:02:59 +0900

    色々追加したよｎ

--- scene_select.cpp
+++ scene_select.cpp
@@ -4,6 +4,7 @@
 int select_state;
 
 Sprite* sprSelect;
+Sprite* sprLock;
 
 void select_init()
 {
@@ -13,6 +14,7 @@ void select_init()
 void select_deinit()
 {
     safe_delete(sprSelect);
+    safe_delete(sprLock);
 }
 
 void select_update()
@@ -22,6 +24,7 @@ void select_update()
     case 0:
         ///// 初期設定 /////
         sprSelect = sprite_load(L"./Data/Images/select.png");
+        sprLock = sprite_load(L"./Data/Images/lock.png");
 
         ++select_state;
     case 1:
@@ -42,6 +45,16 @@ void select_update()
             break;
         }
 
+        // マウスカーソル
+#ifdef _DEBUG
+        std::ostringstream oss;                                 // 文字列ストリーム
+        POINT point;                                            // 位置用の変数を宣言する
+        GetCursorPos(&point);                                   // スクリーン座標を取得する
+        ScreenToClient(window::getHwnd(), &point);              // クライアント座標に変換する
+        oss << "(x=" << point.x << " y=" << point.y << ")";
+        SetWindowTextA(window::getHwnd(), oss.str().c_str());   // タイトルバーにを表示させる
+#endif
+
         break;
     }
 }
@@ -51,4 +64,7 @@ void select_render()
     GameLib::clear(1, 1, 1);
 
     sprite_render(sprSelect, 0, 0);
+
+
+    sprite_render(sprLock, 175.0f, 335.0f, 1.0f, 1.0f, 0, 0);//, 105.0f, 105.0f, 52.5f, 52.5f);
 }
--- scene_tutorial.cpp
+++ scene_tutorial.cpp
@@ -20,15 +20,30 @@ void tutorial_update()
     switch (tutorial_state)
     {
     case 0:
+        ///// ‰Šú‰» /////
+        sprTutorial = sprite_load(L"./Data/Images/tutorial.png");
+
         ++tutorial_state;
     case 1:
+        ///// ƒpƒ‰ƒ[ƒ^‚Ìİ’è
         ++tutorial_state;
     case 2:
+        ///// ’Êí /////
+
+        // ‰æ–ÊØ‚è‘Ö‚¦
+        if (TRG(0) & PAD_SELECT)
+        {
+            nextScene = SCENE::TITLE;
+            break;
+        }
+
         break;
     }
 }
 
 void tutorial_render()
 {
+    GameLib::clear(1, 1, 1);
 
+    sprite_render(sprTutorial, 0, 0);
 }
