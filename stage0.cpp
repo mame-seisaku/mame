0bbd96d1152648b97c98eeab9ee4e9d5eb55b812
mame0622 <mame.like.ww@gmail.com>
2022-10-25 13:18:17 +0900

    stage選択できるように

--- stage0.cpp
+++ stage0.cpp
@@ -1,21 +1,40 @@
 #include "all.h"
 
+/*****•Ï”*****/
+Sprite* spr1;
+
 void stage0_init()
 {
-
+    stage_state[0] = 0;
 }
 
 void stage0_deinit()
 {
-
+    safe_delete(spr1);
 }
 
 void stage0_update()
 {
+    switch (stage_state[0])
+    {
+    case 0:
+        ///// ‰Šúİ’è /////
+        spr1 = sprite_load(L"./Data/Images/01.png");
 
+        ++stage_state[0];
+    case 1:
+        ///// ƒpƒ‰ƒ[ƒ^‚Ìİ’è /////
+        ++stage_state[0];
+    case 2:
+        ///// ’Êí /////
+
+        break;
+    }
 }
 
 void stage0_render()
 {
+    GameLib::clear(1, 1, 1);
 
+    sprite_render(spr1, 0, 0);
 }
